ccd436c2fbc5d6bb57886c9e2a0e5a67b77bb930
aarushi211 <aarushi.jain211@gmail.com>
2025-12-05 02:00:17 -0800

    Updated pipeline

--- src/RAG/rag_pipeline.py
+++ src/RAG/rag_pipeline.py
@@ -4,16 +4,32 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from llama_cpp import Llama
+from pathlib import Path
 
 class TOSAssistant:
     def __init__(self, model_path):
         self.llm = Llama(
             model_path, 
             n_ctx = 8192,
-            n_gpu_layers=-1,
+            n_gpu_layers=0,
             verbose=False
         )
-        self.embed_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+        SCRIPT_DIR = Path(__file__).resolve().parent
+        PROJECT_ROOT = SCRIPT_DIR.parent.parent
+        local_embed_path = PROJECT_ROOT / "models" / "embeddings"
+        # local_embed_path = 'models/embeddings/'
+        # Check if folder exists
+        if local_embed_path.exists():
+            print(f"Loading Embeddings from local container cache at {local_embed_path}...")
+            # self.embed_model = HuggingFaceEmbeddings(model_name=str(local_embed_path))
+            self.embed_model = HuggingFaceEmbeddings(
+                model_name=str(local_embed_path),
+                model_kwargs={"trust_remote_code": True}
+            )
+        else:
+            print("Loading Embeddings from HuggingFace Hub...")
+            # self.embed_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         
         self.vector_store = None
         self.full_text = ""
@@ -36,33 +52,31 @@ class TOSAssistant:
         self.vector_store = FAISS.from_documents(chunks, self.embed_model)
         print("Document ingested and Vector Store created.")
 
-    def format_llama3_prompt(self, system_msg, user_msg):
+    def format_qwen_prompt(self, system_msg, user_msg):
         return (
-            f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n"
-            f"{system_msg}<|eot_id|>"
-            f"<|start_header_id|>user<|end_header_id|>\n"
-            f"{user_msg}<|eot_id|>"
-            f"<|start_header_id|>assistant<|end_header_id|>\n"
+            f"<|im_start|>system\n{system_msg}<|im_end|>\n"
+            f"<|im_start|>user\n{user_msg}<|im_end|>\n"
+            f"<|im_start|>assistant\n"
         )
     
     def generate_global_summary(self):
         if not self.full_text:
             return "No document loaded."
 
-        truncated_text = self.full_text[:30000] 
+        truncated_text = self.full_text[:25000] 
 
         # 1. The Fine-Tuning Prompt (Recreating the magic)
         system_prompt = "You are a legal expert. Summarize the following Terms of Service. Focus on user rights and data privacy."
         user_prompt = f"Summarize the following {self.doc_type} for the service \"{self.service_name}\":\n\nDocument Text:\n{truncated_text}"
 
-        formatted_prompt = self.format_llama3_prompt(system_prompt, user_prompt)
+        formatted_prompt = self.format_qwen_prompt(system_prompt, user_prompt)
 
         # 2. Generate
         output = self.llm(
             formatted_prompt,
-            max_tokens=500, # The abstractive summary length
+            max_tokens=600, # The abstractive summary length
             temperature=0.1,
-            stop=["<|eot_id|>"],
+            stop=["<|im_end|>"],
             echo=False
         )
         return output['choices'][0]['text']
@@ -79,14 +93,14 @@ class TOSAssistant:
         system_prompt = "You are a legal expert assistant. Answer the user's question based strictly on the provided context clauses."
         user_prompt = f"Question: {query}\n\nContext Clauses:\n{context}\n\nAnswer:"
 
-        formatted_prompt = self.format_llama3_prompt(system_prompt, user_prompt)
+        formatted_prompt = self.format_qwen_prompt(system_prompt, user_prompt)
 
         # 3. Generate
         output = self.llm(
             formatted_prompt,
             max_tokens=300,
             temperature=0.2,
-            stop=["<|eot_id|>"],
+            stop=["<|im_end|>"],
             echo=False
         )
         return output['choices'][0]['text']
